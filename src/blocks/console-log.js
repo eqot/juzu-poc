414dba3959b6a79545f778dc7eef5545182b950e
Ikuo Terado <eqobar@gmail.com>
2016-04-02 02:03:23 +0900

    Add ConsoleLog

---
+++ src/blocks/console-log.js
@@ -0,0 +1,8 @@
+export default class ConsoleLog {
+  run (params) {
+    return new Promise((resolve, reject) => {
+      console.log(params)
+      resolve()
+    })
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import ConsoleLog from './blocks/console-log'
+
+const consoleLog = new ConsoleLog();
+
+[1, 2, 3].map(consoleLog.run)
