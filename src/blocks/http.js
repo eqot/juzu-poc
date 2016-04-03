33cd6dbee63a180542bee2beb676763e273a3b7b
Ikuo Terado <eqobar@gmail.com>
2016-04-03 23:50:42 +0900

    Support https in Http

--- src/blocks/http.js
+++ src/blocks/http.js
@@ -1,9 +1,12 @@
 import http from 'http'
+import https from 'https'
 
 export default class Http {
   run (url) {
     return new Promise((resolve, reject) => {
-      http.get(url, (res) => {
+      const connection = url.match(/^https/) ? https : http
+
+      connection.get(url, (res) => {
         let body = ''
 
         res.on('data', (chunk) => {
