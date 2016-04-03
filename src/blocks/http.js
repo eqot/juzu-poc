84bd10fdf123663e7076ec0f02fe01d981b41422
Ikuo Terado <eqobar@gmail.com>
2016-04-03 23:14:08 +0900

    Add Http

---
+++ src/blocks/http.js
@@ -0,0 +1,23 @@
+import http from 'http'
+
+export default class Http {
+  run (url) {
+    return new Promise((resolve, reject) => {
+      http.get(url, (res) => {
+        let body = ''
+
+        res.on('data', (chunk) => {
+          body += chunk
+        })
+
+        res.on('end', () => {
+          if (body.length > 0) {
+            resolve(body)
+          }
+        })
+      }).on('error', (e) => {
+        reject(ee)
+      })
+    })
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import Map from './blocks/map'
+import Http from './blocks/http'
 import ConsoleLog from './blocks/console-log'
 
 const map_x2 = new Map(value => value * 2)
+const http = new Http()
 const consoleLog = new ConsoleLog()
 
 Promise.resolve([1, 2, 3])
   .then(map_x2.run.bind(map_x2))
+  .then(consoleLog.run)
+
+Promise.resolve('http://httpbin.org/get')
+  .then(http.run)
   .then(consoleLog.run)
