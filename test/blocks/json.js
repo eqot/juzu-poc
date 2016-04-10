47a021fd875abad6b751356ec92ebcceb87b0a5b
Ikuo Terado <eqobar@gmail.com>
2016-04-10 14:07:25 +0900

    Add test for Json

--- test/blocks/json.js
+++ test/blocks/json.js
@@ -7,4 +7,16 @@ describe('Json', () => {
   it('returns parse data correctly', () => {
     expect(json.run('{"foo": 123}')).to.deep.equal({ foo: 123 })
   })
+
+  it('returns null if string is null', () => {
+    expect(json.run(null)).to.be.null
+  })
+
+  it('throws SyntaxError exception if string is undfined', () => {
+    expect(json.run.bind(json, undefined)).to.throw(SyntaxError)
+  })
+
+  it('throws SyntaxError exception if string is invalid JSON', () => {
+    expect(json.run.bind(json, 'not valid')).to.throw(SyntaxError)
+  })
 })
